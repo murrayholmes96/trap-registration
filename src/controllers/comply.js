2cf81a1b6ee99320e60249e21afd6bf6f13d262f
Mike Coats <mike.coats@nature.scot>
2020-01-16 12:44:09 +0000

    Update the comply controller to use the checkbox
    The previous radiobutton returned `'yes'` or `'no'` against the comply
    form field. The new checkbox returns `undefined` or `'yes'`, so add an
    extra check for that.
    
    Issue: Scottish-Natural-Heritage/Licensing#146

--- src/controllers/comply.js
+++ src/controllers/comply.js
@@ -2,11 +2,11 @@ import {ReturnState} from './_base.js';
 
 const complyController = (req) => {
   // Did the user tell us they'll comply with the terms.
-  if (req.body.comply === 'yes') {
+  if (req.body.comply !== undefined && req.body.comply === 'yes') {
     // Then we don't have any errors. This clears any previous errors.
     req.session.complyError = false;
-    // Save the decision.
-    req.session.comply = req.body.comply;
+    // Save the agreement to comply.
+    req.session.comply = 'yes';
     // Follow the 'happy path'.
     return ReturnState.Positive;
   }
