5637460dca730b322e63479b5538a512bd36d3d9
John Moore <john.moore@snh.gov.uk>
2020-08-31 15:21:38 +0100

    Add `/conviction` tests
    Issue: Scottish-Natural-Heritage/Licensing#387

---
+++ cypress/integration/003b-conviction-stop.js
@@ -0,0 +1,31 @@
+describe('Conviction stop page directly', function () {
+  it('should prevent access', function () {
+    cy.visit('/conviction-stop', {failOnStatusCode: false});
+    cy.get('h1').should('contain', 'there is a problem with the service');
+  });
+});
+
+describe('Conviction stop page ', function () {
+  beforeEach(() => {
+    // GET `/start`
+    cy.visit('/start');
+
+    // POST `/start`
+    cy.get('#main-content form button.govuk-button').click();
+
+    // ~GET `/gdpr`~
+    // POST `/gdpr`
+    cy.get('#main-content form button.govuk-button').click();
+
+    // ~GET `/conviction`~
+    // CLICK no
+    cy.get('#main-content form input[type="radio"][value="yes"]').click();
+    // POST `/conviction`
+    cy.get('#main-content form button.govuk-button').click();
+  });
+
+  it('should allow access if the user visits all the pages in order', function () {
+    cy.visit('/conviction-stop');
+    cy.get('h1').should('contain', 'You are not eligible');
+  });
+});
